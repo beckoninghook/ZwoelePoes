cc6d6f8047fa81f413ba2db27f6726c04760739d
beckoninghook <beckoninghook@gmail.com>
2016-06-13 21:10:05 +0200

    added some more jquery

---
+++ Niels/NIELSPAGE.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<title>Niels</title>
+<link  href="Stylesheet.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <nav></nav>
+    <div></div>
+    <div class="sect sectOne">
+        <br />
+        <p id="title">Niels van Oeffel</p>
+    </div>
+    <div class="subSection"></div>
+    <div class="sect sectTwo"></div>
+    <div class="subSection footer"></div>
+</body>
+
+</html>
--- Niels/js/script.js
+++ Niels/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
-    $('#title').fadeIn(2000);
+    $('.title').fadeIn(2000);
 });
 
+$(document).ready(function () {
+    $('.uilfoto').fadeIn(2000);
+});
+
+var divs = $('.uilfoto,.title');
+$(window).scroll(function(){
+   if($(window).scrollTop()<10){
+         divs.fadeIn(500);
+   } else {
+         divs.fadeOut(500);
+   }
+});
