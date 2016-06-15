2e5e07b444ba21208ffc42512a0621fb62e46c0e
beckoninghook <beckoninghook@gmail.com>
2016-06-15 22:47:32 +0200

    added all the files

---
+++ php.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php</title>
+</head>
+<body>
+
+<?php
+	
+	$loggedIn = true;
+
+	if ($loggedIn == true)
+	{
+		echo "you're logged in!";
+	}
+	else{
+		echo "you're not logged in!";
+	}
+
+
+?>
+
+</body>
+</html>
